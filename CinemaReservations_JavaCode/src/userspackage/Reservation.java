a5c92d4cf5d38b32ae596113fbcd1d6ce6c41e58
Syrko <kostassir@yahoo.gr>
2019-06-06 00:44:06 +0300

    Made corrections

--- CinemaReservations_JavaCode/src/userspackage/Reservation.java
+++ CinemaReservations_JavaCode/src/userspackage/Reservation.java
@@ -9,29 +9,25 @@ public class Reservation {
 	
 	private Provoli provoli;
 	private int numberOfSeats;
-	private LocalDate reservationDate;
 	private String reservationID;
 	
 	// Constructor
-	public Reservation(String reservationID, Provoli provoli, int numberOfSeats, LocalDate reservationDate) {
+	public Reservation(String reservationID, Provoli provoli, int numberOfSeats) {
 		
 		setReservationID(reservationID);
 		setProvoli(provoli);
 		setNumberOfSeats(numberOfSeats);
-		setReservationDate(reservationDate);
 		
-		System.out.println("Your reservation for " + provoli.getProvoliFilm().getFilmTitle() + " on " + getReservationDate() + " was successful!");
+		System.out.println("Your reservation for " + provoli.getProvoliFilm().getFilmTitle() + " on " + provoli.getProvoliStartDate() + " was successful!");
 	}
 	
 	// Getters
 	public Provoli getProvoli() { return this.provoli; }
 	public int getNumberOfSeats() { return this.numberOfSeats; }
-	public String getReservationDate() { return this.reservationDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")); }
 	public String getReservationID() { return this.reservationID; }
 	
 	// Setters
 	public void setProvoli(Provoli provoli) { this.provoli = provoli; }
 	public void setNumberOfSeats(int numberOfSeats) { this.numberOfSeats = numberOfSeats; }
-	public void setReservationDate(LocalDate reservationDate) { this.reservationDate = reservationDate; }
 	public void setReservationID(String reservationID) { this.reservationID = reservationID; }
 }
